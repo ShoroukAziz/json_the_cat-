b1bf51c85888ffcc8a0ceb04c4395c2ff092cd45
ShoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-02-16 21:03:01 -0700

    refactor the main function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,29 @@
 const request = require('request');
 
-const baseURL = 'https://api.thecatapi.com/v1/breeds/search?q=';
-const searchQuery = process.argv[2];
-const URL = `${baseURL}${searchQuery}`;
 
-request(URL, (error, response, body) => {
 
-  if (error) {
-    console.log('Something went wrong');
-    console.log(error);
-    return;
-  }
+const fetchBreedDescription = (breedName, callback) => {
 
-  const bodyObj = JSON.parse(body);
-  if (bodyObj.length === 0) {
-    console.log('The requested breed is not found! try again');
-    return;
-  }
-  const describtion = bodyObj[0].description;
-  console.log(describtion);
+  const baseURL = 'https://api.thecatapi.com/v1/breeds/search?q=';
+  const URL = `${baseURL}${breedName}`;
 
-});
+  request(URL, (error, response, body) => {
+
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    const bodyObj = JSON.parse(body);
+    if (bodyObj.length === 0) {
+      callback(null, 'The requested breed is not found! try again');
+      return;
+    }
+    callback(null, bodyObj[0].description);
+
+  });
+
+
+};
+
+module.exports = { fetchBreedDescription };
